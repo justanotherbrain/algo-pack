153a6f6e6dff7f02a8b27aef5ba28fb0368f110f
rabadi <rabadi@justanotherbrain.com>
2016-03-03 11:29:43 -0500

    dfs

---
+++ algorithms/DFS.h
@@ -0,0 +1,17 @@
+#ifndef DFS_H
+#define DFS_H
+
+#include "Graph.h"
+
+void DFS( AdjList G, Node *v ){
+  Node *w;
+ 
+  v->explored = true;
+  
+  for (int i(0); i < v->GetNumAdjacent(); i++){
+    w = G.GetNodePointer( v->GetAdjPointer()[i].GetKey() );
+    if (w->explored == false) DFS( G, w );
+  }
+}
+
+#endif
---
+++ demos/algo/DFSDemo.cpp
@@ -0,0 +1,17 @@
+#include "DFS.h"
+#include "Graph.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+  int N = 4;
+  AdjList G(N);
+  G.AppendNode( 0, 1 );
+  G.AppendNode( 0, 2 );
+  G.AppendNode( 1, 3 );
+  G.AppendNode( 2, 3 );
+
+  Node *s = G.GetNodePointer( 0 );
+  DFS( G, s );
+}
+
